b1e1d1b41fb984f58862a946d2734172871b5ac5
connortrue <connor.true.94@gmail.com>
2023-09-06 16:48:23 -0500

    old

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,11 @@
-#!/usr/bin/python3
 import sys
 
-
-def main():
-    num = len(sys.argv) - 1
-    
-    if num >= 0:
-        print("{:d} arguments:".format(num))
-        for i in range(1, num + 1):
-            print("{:d}: {:s}".format(i, sys.argv[i]))
-
+def print_args():
+    args = sys.argv[1:]
+    num_args = len(args)
+    print(f"{num_args} argument{'s' if num_args != 1 else ''}:")
+    for i, arg in enumerate(args):
+        print(f"{i+1}: {arg}")
 
 if __name__ == "__main__":
-    main()
+    print_args()
