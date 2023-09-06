da06a89aceb938b0d87d788d9b7487b026537882
connortrue <connor.true.94@gmail.com>
2023-09-06 16:37:06 -0500

    why cant i see the requirements anymore

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ import sys
 def print_args():
     args = sys.argv[1:]
     num_args = len(args)
-    print(f"Number of argument{'s' if num_args != 1 else ''}: {num_args}")
+    print(f"{num_args} argument{'s' if num_args != 1 else ''}:")
     for i, arg in enumerate(args):
         print(f"{i+1}: {arg}")
 
