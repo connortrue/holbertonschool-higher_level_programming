b18fa079692a94730cf27af51397fda4ea7bb709
connortrue <connor.true.94@gmail.com>
2023-09-06 15:08:58 -0500

    glad new lines work how they do

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+result = ""
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{0}{1}, ".format(i, j), end="")
-print()
+        result += "{0}{1}, ".format(i, j)
+print(result[:-2])
