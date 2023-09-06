04385ba68053fac4d8cdd94cc2593895873d476f
connortrue <connor.true.94@gmail.com>
2023-09-06 16:55:48 -0500

    pycodestyle

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,14 @@
-#!/usr/bin/python3
 import hidden_4
 
+
 def print_names():
+    """Prints names defined by the compiled module hidden_4.pyc"""
     names = dir(hidden_4)
     for name in sorted(names):
         if not name.startswith("__"):
             print(name)
 
+
 if __name__ == "__main__":
     print_names()
+
