fa013a4b8c11204fc3d213d7b6bf4081904094bd
connortrue <connor.true.94@gmail.com>
2023-09-06 17:02:57 -0500

    pycodestyles

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import sys
 
+
 def print_args():
     args = sys.argv[1:]
     num_args = len(args)
     print(f"{num_args} argument{'s' if num_args != 1 else ''}:")
     for i, arg in enumerate(args):
         print(f"{i+1}: {arg}")
+
 
 if __name__ == "__main__":
     print_args()
