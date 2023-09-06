57a7dc9fe6cbebda68788e2c31828937ba307c2b
connortrue <connor.true.94@gmail.com>
2023-09-06 16:44:47 -0500

    fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ import sys
 def main():
     num = len(sys.argv) - 1
     
-    if num >= 1:
+    if num >= 0 and not sys.argv[1]:
         print("{:d} arguments:".format(num))
         for i in range(1, num + 1):
             print("{:d}: {:s}".format(i, sys.argv[i]))
