2bd375b37085391a3cca3d2ab897f41a3beef987
connortrue <connor.true.94@gmail.com>
2023-09-06 13:33:46 -0500

    doesnt make sense

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,13 +7,10 @@ last = number % 10
 
 # Construct the output string based on the last digit
 if last > 5:
-    output = f"Last digit of {number} is {last} and is greater than 5"
+    print(f"Last digit of {number} is {last} and is greater than 5")
 elif last == 0:
-    output = f"Last digit of {number} is {last} and is 0"
+    print(f"Last digit of {number} is {last} and is 0")
 elif last < 6:
-    output = f"Last digit of {number} is {last} and is less than 6 and not 0"
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
 else:
-    output = f"boi you messed up"
-
-# Print the final output
-print(output)
+    print(f"boi you messed up")
