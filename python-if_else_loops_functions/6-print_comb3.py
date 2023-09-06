6f6db83093d19c3e2596b5c414c6a8d21f34bca1
connortrue <connor.true.94@gmail.com>
2023-09-06 14:18:39 -0500

    newvway

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-result = []
-
-for i in range(len(digits)):
-    for j in range(i + 1, len(digits)):
-        if digits[i] < digits[j]:
-            result.append("{0}{1:02d}".format(digits[i], digits[j]))
-
-print(", ".join(result))
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{0}{1}, ".format(i, j), end="")
+print()
