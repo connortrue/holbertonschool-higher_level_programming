71d0288249063cb03755db01886b44b41aa33ae8
connortrue <connor.true.94@gmail.com>
2023-09-06 17:11:00 -0500

    bonus check

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,9 +5,12 @@ import sys
 def print_args():
     args = sys.argv[1:]
     num_args = len(args)
-    print(f"{num_args} argument{'s' if num_args != 1 else ''}:")
-    for i, arg in enumerate(args):
-        print(f"{i+1}: {arg}")
+    if num_args == 0:
+        print("0 arguments.")
+    else:
+        print(f"{num_args} argument{'s' if num_args != 1 else ''}:")
+        for i, arg in enumerate(args):
+            print(f"{i+1}: {arg}")
 
 
 if __name__ == "__main__":
