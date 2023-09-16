c01871dc405f6293e651e1feaf7dc0f9bde36a46
connortrue <connor.true.94@gmail.com>
2023-09-16 14:00:03 -0500

    docs

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 class Square:
+    """
+    Square is a class that defines a square.
+
+    Attributes:
+        __size (int): size of a side of the square.
+    """
     pass
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 class Square:
+    """
+    Square is a class that defines a square.
+
+    Attributes:
+        __size (int): size of a side of the square.
+    """
+
     def __init__(self, size):
+        """
+        The constructor for Square class.
+
+        Parameters:
+           size (int): The size of a side of the square.
+        """
         self.__size = size
