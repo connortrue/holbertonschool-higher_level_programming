90a7ad4ad4d3e76bfb796e5948430c021ef37c80
connortrue <connor.true.94@gmail.com>
2023-09-19 13:49:58 -0500

    5 wow had to assign it

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -16,7 +16,7 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    text.replace(" ", "")
+    text = text.replace(" ", "")
     for char in text:
         print(char, end='')
         if char in ['.', '?', ':']:
