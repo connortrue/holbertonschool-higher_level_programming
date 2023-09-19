56384cf5142bf1622c90ebf114b2499a3c0c1a5b
connortrue <connor.true.94@gmail.com>
2023-09-19 13:10:39 -0500

    5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# 2-matrix_divided.py
+# Connor True
+"""Doc"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ., ? and :.
+
+    Args:
+        text (str): The input text.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for char in text:
+        print(char, end='')
+        if char in ['.', ',', '?', ':']:
+            print("\n\n")
