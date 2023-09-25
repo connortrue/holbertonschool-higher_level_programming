5430ba954e61dcd32d6ace31fa8b8512fc4aba90
connortrue <connor.true.94@gmail.com>
2023-09-24 19:14:15 -0500

    doc

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+"""Doc"""
+
+
 def is_same_class(obj, a_class):
     return type(obj) is a_class
--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+"""Doc"""
+
+
 def is_kind_of_class(obj, a_class):
     return isinstance(obj, a_class)
