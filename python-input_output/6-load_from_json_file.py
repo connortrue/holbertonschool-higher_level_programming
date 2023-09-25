e297c0bb6f85ea28fbda0f66199b809b7db8c2a6
connortrue <connor.true.94@gmail.com>
2023-09-25 11:08:05 -0500

    6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Doc"""
+
+
+import json
+
+def load_from_json_file(filename):
+    """
+    This function creates an object from a JSON file.
+
+    Args:
+        filename: The name of the JSON file.
+
+    Returns:
+        An object represented by the JSON string in the file.
+
+    Note:
+        This function uses the 'with' statement for proper file handling.
+        It does not handle exceptions if the JSON string doesn't represent
+        an object or file permission exceptions.
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
