887a87d00a979fbe0632201581f2bac12c4baa0d
connortrue <connor.true.94@gmail.com>
2023-09-25 11:24:30 -0500

    11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Doc"""
+
+
+class Student:
+    """
+    Defines a student with first name, last name, and age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new instance of the Student class
+
+        Parameters:
+        first_name (str): The first name of the student
+        last_name (str): The last name of the student
+        age (int): The age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance
+
+        Parameters:
+        attrs (list): A list of strings representing attribute names
+
+        Returns:
+        dict: A dictionary representation of the Student instance
+        """
+
+        if attrs is None:
+            return {key: value for key, value in self.__dict__.items() if
+                    isinstance(value, (list, dict, str, int, bool))}
+
+        return {key: value for key, value in self.__dict__.items() if key in
+                attrs and isinstance(value, (list, dict, str, int, bool))}
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance
+
+        Parameters:
+        json (dict): A dictionary with attribute names as keys and new values
+        for those attributes
+        """
+
+        for key in json:
+            if hasattr(self, key):
+                setattr(self, key, json[key])
