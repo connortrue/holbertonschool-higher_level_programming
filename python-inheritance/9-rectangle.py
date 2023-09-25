0ff333a10f7758fe19b05b9436e7e0451a2c286e
connortrue <connor.true.94@gmail.com>
2023-09-24 19:33:31 -0500

    9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Doc"""
+
+
+class Rectangle(BaseGeometry):
+    """Doc"""
+
+    def __init__(self, width, height):
+        """Doc"""
+
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
+
+    def area(self):
+        """Doc"""
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Doc"""
+
+        return f"[Rectangle] {self.__width}/{self.__height}"
