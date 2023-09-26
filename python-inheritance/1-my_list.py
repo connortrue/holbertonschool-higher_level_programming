28845d39a2df525f2e2b58faa594195c84482df2
connortrue <connor.true.94@gmail.com>
2023-09-26 16:30:11 -0500

    doc

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-"""Doc"""
+"""MyList is a custom list class that provides additional functionality,
+such as printing the list elements sorted."""
 
 
 class MyList(list):
-    """Doc"""
+    """
+    MyList is a custom list class that provides additional functionality,
+    such as printing the list elements sorted.
+    """
 
     def print_sorted(self):
+        """
+        Prints the sorted elements of the list.
+        """
         print(sorted(list(self)))
