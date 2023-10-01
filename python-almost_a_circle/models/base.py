031af7c5ddc30fbe462f7d2cca4283fec07df1e0
connortrue <connor.true.94@gmail.com>
2023-10-01 10:58:46 -0500

    there

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
-"""Doc"""
+"""This is documentation for a class project. Please don't read too much into
+all of this, I'm only on month one of python training here. Hi Travis."""
 
 
 class Base:
-    """Doc"""
+    """
+    This class serves as the base class for all other classes in the project.
+    It manages the 'id' attribute in all future classes, preventing code duplication.
+    """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Doc"""
+        """
+        Constructor method for the Base class.
+
+        Parameters:
+        id (int, optional): The id of the instance. If not provided, it will be incremented
+        and assigned automatically.
+        """
         if id is not None:
             self.id = id
         else:
