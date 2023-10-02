134ac0ab6d81345bf24a4ba60ef9150e824f072e
connortrue <connor.true.94@gmail.com>
2023-10-02 14:19:23 -0500

    dict to instance

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -81,3 +81,24 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Returns an instance with all attributes already set.
+
+        Parameters:
+        **dictionary: A double pointer to a dictionary.
+
+        Returns:
+        An instance with all attributes already set.
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            dummy = cls()
+
+        dummy.update(**dictionary)
+        return dummy
