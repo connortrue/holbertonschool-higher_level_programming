5d607c684c6ef74743dbbaf5186c8c5d094bd63c
connortrue <connor.true.94@gmail.com>
2023-10-03 22:08:16 -0500

    tests

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -3,78 +3,60 @@
 all of this, I'm only on month one of python training here."""
 import unittest
 from models.rectangle import Rectangle
+import json
 
 
 class TestRectangle(unittest.TestCase):
-    """These are tests for the rectangle class. I don't know how much text you
-    really need for a doc but I want at least two lines."""
-
-    def test_setUp(self):
-        """Set up for the tests."""
-        self.rect1 = Rectangle(5, 4)
-        self.rect2 = Rectangle(2, 3, 1, 3, 12)
+    """These are unit tests for the Rectangle class."""
 
     def test_id_assignment(self):
         """Test for id assignment."""
-        self.assertEqual(self.rect1.id, 1)
-        self.assertEqual(self.rect2.id, 12)
-
-    def test_width_assignment(self):
-        """Test for width assignment."""
-        self.assertEqual(self.rect1.width, 5)
-        self.assertEqual(self.rect2.width, 2)
-
-    def test_height_assignment(self):
-        """Test for height assignment."""
-        self.assertEqual(self.rect1.height, 4)
-        self.assertEqual(self.rect2.height, 3)
-
-    def test_x_assignment(self):
-        """Test for x assignment."""
-        self.assertEqual(self.rect1.x, 0)
-        self.assertEqual(self.rect2.x, 1)
-
-    def test_y_assignment(self):
-        """Test for y assignment."""
-        self.assertEqual(self.rect1.y, 0)
-        self.assertEqual(self.rect2.y, 3)
+        rect1 = Rectangle(5, 4)
+        rect2 = Rectangle(2, 3, 1, 1, 12)
+        rect3 = Rectangle(7, 8)
+        self.assertEqual(rect1.id, 1)
+        self.assertEqual(rect2.id, 12)
+        self.assertEqual(rect3.id, 2)
 
     def test_area(self):
         """Test for area method."""
-        self.assertEqual(self.rect1.area(), 20)
-        self.assertEqual(self.rect2.area(), 6)
-
-    def test_update(self):
-        """Test for update method."""
-        self.rect1.update(89, 2, 3)
-        self.assertEqual(self.rect1.id, 89)
-        self.assertEqual(self.rect1.width, 2)
-        self.assertEqual(self.rect1.height, 3)
+        rect1 = Rectangle(5, 4)
+        rect2 = Rectangle(2, 3)
+        self.assertEqual(rect1.area(), 20)
+        self.assertEqual(rect2.area(), 6)
 
     def test_to_dictionary(self):
         """Test for to_dictionary method."""
-        self.assertEqual(self.rect1.to_dictionary(),
+        rect1 = Rectangle(5, 4)
+        self.assertEqual(rect1.to_dictionary(),
                          {'id': 1, 'width': 5, 'height': 4, 'x': 0, 'y': 0})
 
-    def test_invalid_width(self):
-        """Test for invalid width."""
-        with self.assertRaises(TypeError):
-            Rectangle("invalid", 2)
+    def test_update(self):
+        """Test for update method."""
+        rect1 = Rectangle(5, 4)
+        rect1.update(89, 2, 3)
+        self.assertEqual(rect1.id, 89)
+        self.assertEqual(rect1.width, 2)
+        self.assertEqual(rect1.height, 3)
 
-    def test_invalid_height(self):
-        """Test for invalid height."""
-        with self.assertRaises(TypeError):
-            Rectangle(2, "invalid")
+    def test_save_to_file(self):
+        """Test for save_to_file method."""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+        with open("Rectangle.json", "r") as file:
+            self.assertEqual(json.dumps([r1.to_dictionary(),
+                                        r2.to_dictionary()]), file.read())
 
-    def test_invalid_x(self):
-        """Test for invalid x."""
-        with self.assertRaises(TypeError):
-            Rectangle(2, 3, "invalid")
+    def test_load_from_file(self):
+        """Test for load_from_file method."""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
 
-    def test_invalid_y(self):
-        """Test for invalid y."""
-        with self.assertRaises(TypeError):
-            Rectangle(2, 3, 1, "invalid")
+        load_rectangles = Rectangle.load_from_file()
+        self.assertEqual(r1.__dict__, load_rectangles[0].__dict__)
+        self.assertEqual(r2.__dict__, load_rectangles[1].__dict__)
 
 
 if __name__ == '__main__':
--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -6,43 +6,47 @@ from models.square import Square
 
 
 class TestSquare(unittest.TestCase):
-    """This is unit tests for the square case. I don't know how much more
-    documentation you require. Beep Boop."""
-
-    def test_setUp(self):
-        """Set up for the tests."""
-        self.square1 = Square(5)
-        self.square2 = Square(2, 1, 3, 12)
+    """This is unit tests for the square case."""
 
     def test_id_assignment(self):
         """Test for id assignment."""
-        self.assertEqual(self.square1.id, 1)
-        self.assertEqual(self.square2.id, 12)
+        square1 = Square(5)
+        square2 = Square(2, 1, 3, 12)
+        self.assertEqual(square1.id, 1)
+        self.assertEqual(square2.id, 12)
 
     def test_size_assignment(self):
         """Test for size assignment."""
-        self.assertEqual(self.square1.size, 5)
-        self.assertEqual(self.square2.size, 2)
+        square1 = Square(5)
+        square2 = Square(2, 1, 3, 12)
+        self.assertEqual(square1.size, 5)
+        self.assertEqual(square2.size, 2)
 
     def test_x_assignment(self):
         """Test for x assignment."""
-        self.assertEqual(self.square1.x, 0)
-        self.assertEqual(self.square2.x, 1)
+        square1 = Square(5)
+        square2 = Square(2, 1, 3, 12)
+        self.assertEqual(square1.x, 0)
+        self.assertEqual(square2.x, 1)
 
     def test_y_assignment(self):
         """Test for y assignment."""
-        self.assertEqual(self.square1.y, 0)
-        self.assertEqual(self.square2.y, 3)
+        square1 = Square(5)
+        square2 = Square(2, 1, 3, 12)
+        self.assertEqual(square1.y, 0)
+        self.assertEqual(square2.y, 3)
 
     def test_update(self):
         """Test for update method."""
-        self.square1.update(89, 2)
-        self.assertEqual(self.square1.id, 89)
-        self.assertEqual(self.square1.size, 2)
+        square1 = Square(5)
+        square1.update(89, 2)
+        self.assertEqual(square1.id, 89)
+        self.assertEqual(square1.size, 2)
 
     def test_to_dictionary(self):
         """Test for to_dictionary method."""
-        self.assertEqual(self.square1.to_dictionary(),
+        square1 = Square(5)
+        self.assertEqual(square1.to_dictionary(),
                          {'id': 1, 'size': 5, 'x': 0, 'y': 0})
 
 
