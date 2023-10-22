c9f42f6c09c8740e7341e55a286853289c527bd0
connortrue <connor.true.94@gmail.com>
2023-10-21 20:53:32 -0500

    5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,11 +1,2 @@
 -- for
-SELECT 
-    COLUMN_NAME, 
-    DATA_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_DEFAULT 
-FROM 
-    INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    TABLE_NAME = 'first_table' AND 
-    TABLE_SCHEMA = 'hbtn_0c_0';
+SHOW CREATE TABLE first_table
