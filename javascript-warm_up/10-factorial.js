dc12998911c75ac2e79d4b8ba017d84376d9d364
connortrue <connor.true.94@gmail.com>
2023-11-25 15:57:38 -0600

    10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+function factorial(n) {
+    if (isNaN(n)) {
+      return 1;
+    }
+    
+    if (n === 0) {
+      return 1;
+    }
+    
+    return n * factorial(n - 1);
+  }
+  
+  const number = parseInt(process.argv[2]);
+  
+  console.log(factorial(number));
