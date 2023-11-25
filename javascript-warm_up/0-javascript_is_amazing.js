7344b3faafdb829883b868399aa02e5d728aae64
connortrue <connor.true.94@gmail.com>
2023-11-25 15:59:00 -0600

    node lines

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,2 +1,3 @@
+#!/usr/bin/node
 const myVar = "<link>JavaScript</link> is amazing";
 console.log(myVar);
--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,3 +1,4 @@
+#!/usr/bin/node
 function factorial(n) {
     if (isNaN(n)) {
       return 1;
