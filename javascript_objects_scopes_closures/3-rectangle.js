4d4382a21319f6adf9a18e23aefd718f4119265f
connortrue <connor.true.94@gmail.com>
2023-11-27 13:27:06 -0600

    3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      // If width or height is not a positive integer or equal to 0, create an empty object
+      this.width = null;
+      this.height = null;
+    } else {
+      // Initialize the instance attributes width and height
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.width === null || this.height === null) {
+      console.log('Empty object');
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
