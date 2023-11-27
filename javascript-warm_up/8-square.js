3b817e973cad0dbafef05ee38e8316d7ebecaef9
connortrue <connor.true.94@gmail.com>
2023-11-27 12:44:38 -0600

    8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const printSquare = (size) => {
+  const sizeInt = parseInt(size);
+
+  if (isNaN(sizeInt)) {
+    console.log('Missing size');
+    return;
+  }
+
+  for (let i = 0; i < sizeInt; i++) {
+    let row = '';
+    for (let j = 0; j < sizeInt; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+};
+
+printSquare(process.argv[2]);
