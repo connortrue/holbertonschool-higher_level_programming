40684a94ebe9a3a2f4361b6dfbf0c178880565c0
connortrue <connor.true.94@gmail.com>
2023-11-27 12:43:42 -0600

    7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const input = process.argv[2];
+const x = parseInt(input);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  let count = 0;
+  while (count < x) {
+    console.log('C is fun');
+    count++;
+  }
+}
