afa8c9c91f90eb375bb47b4748376ea558d61110
connortrue <connor.true.94@gmail.com>
2023-11-27 13:42:40 -0600

    6

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+class Square extends Rectangle {
+  constructor (size) {
+    // Call the constructor of Rectangle
+    super(size, size);
+  }
+}
--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+class Square extends Square {
+  constructor (size) {
+    // Call the constructor of Square
+    super(size);
+  }
+
+  charPrint (c) {
+    // If c is undefined, use 'X'
+    c = c || 'X';
+
+    // If width and height are both 0, do not print anything
+    if (this.width === 0 || this.height === 0) {
+      return;
+    }
+
+    // Print the square using 'c' character
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += c;
+      }
+      console.log(line);
+    }
+  }
+}
