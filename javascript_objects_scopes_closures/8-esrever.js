53aa8a5ed7f362efc0943611e47a5694a39cb5de
connortrue <connor.true.94@gmail.com>
2023-11-27 13:19:57 -0600

    8

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  // Create an empty array to store the reversed list
+  const reversedList = [];
+
+  // Iterate through the input list from the end to the beginning
+  for (let i = list.length - 1; i >= 0; i--) {
+    // Add each element to the reversed list
+    reversedList.push(list[i]);
+  }
+
+  // Return the reversed list
+  return reversedList;
+};
