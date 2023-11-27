2498cb5e2ede2df33624c1688d93e06171894c78
connortrue <connor.true.94@gmail.com>
2023-11-27 13:40:39 -0600

    4

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,41 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    // Check if w and h are positive integers
+    if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // If w or h is not a positive integer, create an empty object
+      this.width = 0;
+      this.height = 0;
+    }
+  }
+
+  print () {
+    // If width and height are both 0, do not print anything
+    if (this.width === 0 || this.height === 0) {
+      return;
+    }
+
+    // Print the rectangle using 'X' character
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += 'X';
+      }
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    // Swap width and height
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    // Multiply width and height by 2
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
