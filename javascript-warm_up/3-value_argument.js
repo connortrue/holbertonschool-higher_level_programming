bc7917f24a3a4008247a3334371c66ae4c167f15
connortrue <connor.true.94@gmail.com>
2023-11-28 21:53:59 -0600

    3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || process.argv[3] === undefined) {
+const args = process.argv.slice(2);
+if (args[0] === undefined) {
   console.log('No argument');
 } else {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
+  console.log(args[0]);
 }
