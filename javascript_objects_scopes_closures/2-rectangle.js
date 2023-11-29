67437dde4076b82f500439856375552ad7832f3d
connortrue <connor.true.94@gmail.com>
2023-11-28 21:20:32 -0600

    2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,24 +1,9 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {
-      this.width = null;
-      this.height = null;
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
     }
   }
-
-  print () {
-    if (this.width === null || this.height === null) {
-      return 'Empty object';
-    } else {
-      let rectangle = '';
-      for (let i = 0; i < this.height; i++) {
-        rectangle += 'X'.repeat(this.width) + '\n';
-      }
-      return rectangle;
-    }
-  }
-}
+};
