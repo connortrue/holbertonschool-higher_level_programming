8ed84127d16c816daad5b0790cd6a9d57d6f4ca9
connortrue <connor.true.94@gmail.com>
2023-12-01 16:57:17 -0600

    2

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('<link>request</link>');
+
+// Get the URL from the command line argument
+const url = process.argv[2];
+
+// Perform the GET request
+request(url, (error, response) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`code: ${response.statusCode}`); // Display the status code
+  }
+});
