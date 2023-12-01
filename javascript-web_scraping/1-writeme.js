e9d9c10c9922b8660a2f9adb0a9472017ff1e195
connortrue <connor.true.94@gmail.com>
2023-12-01 16:52:52 -0600

    1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+// Get the file path and the string to write from command line arguments
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
+
+// Write the string to the file
+fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err); // Print the error object if an error occurred during writing
+  } else {
+    console.log('The file has been saved!'); // Notify that the file has been successfully written
+  }
+});
