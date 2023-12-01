d52329310ace829170d37252068072d0305508be
connortrue <connor.true.94@gmail.com>
2023-12-01 16:51:10 -0600

    0

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+// Get the file path from the command line argument
+const filePath = process.argv[2];
+
+// Read and print the content of the file
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err); // Print the error object if an error occurred during reading
+  } else {
+    console.log(data); // Print the content of the file
+  }
+});
