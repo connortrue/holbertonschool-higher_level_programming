848cae611d86f03eca3ad86cd12ead56e5f799af
connortrue <connor.true.94@gmail.com>
2023-12-04 15:32:57 -0600

    5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+
+// Get command-line arguments
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+// Make a GET request to the URL
+request(url, function (error, response, body) {
+   if (!error && response.statusCode == 200) {
+       // Write the response body to the file
+       fs.writeFile(filePath, body, { encoding: 'utf8' }, function (err) {
+           if (err) throw err;
+           console.log('The file has been saved!');
+       });
+   } else {
+       console.log('Error:', error);
+   }
+});
