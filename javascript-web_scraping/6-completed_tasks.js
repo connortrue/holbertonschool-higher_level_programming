961054d907214aed4f52a382ec6b1e4287665302
connortrue <connor.true.94@gmail.com>
2023-12-05 12:52:42 -0600

    semistandard

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -2,30 +2,30 @@
 const request = require('request');
 
 function getCompletedTasks(url) {
- return new Promise((resolve, reject) => {
-  url = process.argv[2];
-  request(url, (error, response, body) => {
-    if (error) {
-      reject(error);
-    } else {
-      const tasks = JSON.parse(body);
-      const userTasks = tasks.filter(task => task.completed);
-      const userCounts = {};
-
-      userTasks.forEach(task => {
-        if (userCounts[task.userId]) {
-          userCounts[task.userId]++;
+  return new Promise((resolve, reject) => {
+    url = process.argv[2];
+    request(url, (error, response, body) => {
+        if (error) {
+            reject(error);
         } else {
-          userCounts[task.userId] = 1;
-        }
-      });
+            const tasks = JSON.parse(body);
+            const userTasks = tasks.filter(task => task.completed);
+            const userCounts = {};
 
-      console.log(userCounts);
-      resolve(userCounts);
-    }
+            userTasks.forEach(task => {
+                if (userCounts[task.userId]) {
+                    userCounts[task.userId]++;
+                } else {
+                    userCounts[task.userId] = 1;
+                }
+            });
+
+            console.log(userCounts);
+            resolve(userCounts);
+        }
+    });
   });
- });
 }
 
 getCompletedTasks(process.argv[2])
- .catch(error => console.error(error));
+  .catch(error => console.error(error));
