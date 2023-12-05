fe60c5b780dd61e439f0956fa41ee125b9208bde
connortrue <connor.true.94@gmail.com>
2023-12-05 14:13:52 -0600

    semi

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -8,7 +8,7 @@ const filePath = process.argv[3];
 
 // Make a GET request to the URL
 request(url, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
+  if (!error && response.statusCode === 200) {
     // Write the response body to the file
     fs.writeFile(filePath, body, { encoding: 'utf8' }, function (err) {
       if (err) throw err;
--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,27 +1,27 @@
 #!/usr/bin/node
 const request = require('request');
 
-function getCompletedTasks(url) {
+function getCompletedTasks (url) {
   return new Promise((resolve, reject) => {
     url = process.argv[2];
     request(url, (error, response, body) => {
-        if (error) {
-            reject(error);
-        } else {
-            const tasks = JSON.parse(body);
-            const userTasks = tasks.filter(task => task.completed);
-            const userCounts = {};
+      if (error) {
+        reject(error);
+      } else {
+        const tasks = JSON.parse(body);
+        const userTasks = tasks.filter(task => task.completed);
+        const userCounts = {};
 
-            userTasks.forEach(task => {
-                if (userCounts[task.userId]) {
-                    userCounts[task.userId]++;
-                } else {
-                    userCounts[task.userId] = 1;
-                }
-            });
+        userTasks.forEach(task => {
+            if (userCounts[task.userId]) {
+                userCounts[task.userId]++;
+            } else {
+                userCounts[task.userId] = 1;
+            }
+        });
 
-            console.log(userCounts);
-            resolve(userCounts);
+        console.log(userCounts);
+        resolve(userCounts);
         }
     });
   });
