c24cb71938766078c9a2b728fc1108352dcebb89
connortrue <connor.true.94@gmail.com>
2023-12-05 12:56:40 -0600

    semi

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -8,12 +8,12 @@ const filePath = process.argv[3];
 
 // Make a GET request to the URL
 request(url, function (error, response, body) {
-   if (!error && response.statusCode == 200) {
-       // Write the response body to the file
-       fs.writeFile(filePath, body, { encoding: 'utf8' }, function (err) {
-           if (err) throw err;
-       });
-   } else {
-       console.log('Error:', error);
-   }
+  if (!error && response.statusCode == 200) {
+    // Write the response body to the file
+    fs.writeFile(filePath, body, { encoding: 'utf8' }, function (err) {
+      if (err) throw err;
+    });
+  } else {
+    console.log('Error:', error);
+  }
 });
